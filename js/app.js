ca5b09f8fa32f7cb4f2b165365b27d2aaf53f14e
jhoset10 <jhosetceron@aol.com>
2020-04-20 17:34:51 -0400

    Implemented function

---
+++ js/app.js
@@ -0,0 +1,63 @@
+/*********************************
+
+Selectors
+
+*********************************/
+
+// Grabs input by the user
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+/*********************************
+
+Event Listeners
+
+**********************************/
+
+// Adds event listener.
+// adds a todo every time the
+// button is clicked.
+todoButton = addEventListener('click', addTodo)
+
+
+/*********************************
+
+Functions
+
+**********************************/
+
+// Builds function.
+function addTodo(event) {
+
+  // Prevent form from submiting.
+  event.preventDefault();
+
+  // Todo DIV
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  // Creates Li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = "Hey";
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+
+  //Check mark button.
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  // Check trash button.
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<li class="fas fa-trash"></li>'
+  trashButton.classList.add("complete-btn");
+  todoDiv.appendChild(trashButton);
+
+  // Appends to list.
+  todoList.appendChild(todoDiv);
+
+
+
+}
